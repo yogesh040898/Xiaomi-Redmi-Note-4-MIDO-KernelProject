8b0f5891bf0015e35bad25c42dee7c42133407b0
shashank1436 <9945shashank@gmail.com>
2021-03-07 14:20:19 +0100

    Revert "mm: Low Memory Saver"
    This reverts commit e59e121e9528769500b22b0c42299eec467f81ab.

--- kernel/exit.c
+++ kernel/exit.c
@@ -61,7 +61,6 @@
 #include <asm/unistd.h>
 #include <asm/pgtable.h>
 #include <asm/mmu_context.h>
-#include <linux/boost_sigkill_free.h>
 
 static void __unhash_process(struct task_struct *p, bool group_dead)
 {
@@ -943,9 +942,6 @@ do_group_exit(int exit_code)
 		}
 		spin_unlock_irq(&sighand->siglock);
 	}
-
-	if (sig_kernel_kill(exit_code))
-	fast_free_user_mem();
 
 	do_exit(exit_code);
 	/* NOTREACHED */
